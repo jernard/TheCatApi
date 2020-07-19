1dea629b0c76673820e5d1299de1546ad38361ca
Jernard Arturo Ducon Cardona <ECM8643H@claro.com.co>
2020-07-19 15:10:26 -0500

    AppIcon

--- TheCatApi/History/View/HistoryViewController.swift
+++ TheCatApi/History/View/HistoryViewController.swift
@@ -15,6 +15,7 @@ class HistoryViewController: UIViewController {
     
     struct cellsId {
         static let voted = "votedCell"
+        static let noData = "nodata"
     }
     
     override func viewDidLoad() {
@@ -36,22 +37,29 @@ class HistoryViewController: UIViewController {
 }
 extension HistoryViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel?.container.count ?? 0
+        return viewModel?.container.count ?? 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellsId.voted) as! HistoryTableViewCell
         
-        if let voted = viewModel?.container[indexPath.row]{
+        
+        if let voted = viewModel?.container[indexPath.row], viewModel?.container.count ?? 0 > 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellsId.voted) as! HistoryTableViewCell
             cell.setUp(model: voted)
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellsId.noData)!
+            return cell
         }
-        
-        
-        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 301
+        if let _ = viewModel?.container[indexPath.row], viewModel?.container.count ?? 0 > 0{
+            return 301
+        }else{
+            return tableView.frame.height
+        }
+        
     }
     
     
